e032fbdd2950e488ffc1dc438ac1882248d787db
cccnrc <ec3408@cumc.columbia.edu>
2022-10-29 12:04:30 +0200

    commit 0.1

--- R/make_plot.R
+++ R/make_plot.R
@@ -11,8 +11,9 @@
 #' @return The VCF variant plot
 make_plot <- function(MODELED_VCF, SEQINFO, VAR_Y, VAR_FLAG="POS", THRESHOLD=FALSE, CHR_NAMES=c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY")){
   ### if user specified a column to use to plot data use this
+  print( VAR_FLAG )
   if ( VAR_FLAG[1] != "POS" ) {
-    if ( VAR_FLAG %in% colnames(MODELED_VCF) ){
+    if ( VAR_FLAG[1] %in% colnames(MODELED_VCF) ){
       MODELED_VCF  <- MODELED_VCF[ MODELED_VCF[,'CHROM'] %in% CHR_NAMES, c('IND','CHROM','POS', VAR_FLAG)]
       VAR_Y <- MODELED_VCF[,VAR_FLAG]
     }
