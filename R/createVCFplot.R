559d89681e9c04130515d76c136ca8c271b32086
cccnrc <ec3408@cumc.columbia.edu>
2022-10-29 11:35:37 +0200

    commit 0.1

--- R/createVCFplot.R
+++ R/createVCFplot.R
@@ -5,17 +5,17 @@
 #' @param VCF_FILE Path to the input VCF file
 #' @param FASTA_FILE Path to the input FASTA file
 #' @param ASSEMBLY (optional) which assembly your VCF is (hg38/hg19)
-#' @param VALUE (optional) the VCF variable to use as Y-axis for the variants, default is just position
+#' @param VAR_FLAG (optional) the VCF variable to use as Y-axis for the variants, default is just position
 #' @param ORDERED (optional) if user want to have ordered variant height in the plot or random (if value NOT specified)
 #' @param CHR_NAMES (optional) vector with chromosme names to plot
 #' @param VERBOSE (optional) if you want steps printed to stdout
 #' @return the plot
 #' @export
 createVCFplot <- function(VCF_FILE, FASTA_FILE, ASSEMBLY="hg38", VAR_FLAG="POS", ORDERED=FALSE, CHR_NAMES=c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY"), VERBOSE=TRUE){
-  ### cannot use VALUE and ORDERED
-  if (( VALUE != "POS" )&( ORDERED != FALSE )) {
+  ### cannot use VAR_FLAG and ORDERED
+  if (( VAR_FLAG != "POS" )&( ORDERED != FALSE )) {
     cat('\n')
-    cat(' -> can not use both VALUE and ORDERED! (see documentation). Choose only one...\n')
+    cat(' -> can not use both VAR_FLAG and ORDERED! (see documentation). Choose only one...\n')
   }
   if ( VERBOSE == TRUE ) {
     cat('\n')
@@ -36,6 +36,6 @@ createVCFplot <- function(VCF_FILE, FASTA_FILE, ASSEMBLY="hg38", VAR_FLAG="POS",
     cat(' -> creating the plot ...\n')
     cat('\n')
   }
-  PLOT <- make_plot(VCF, SEQ, VALUE, VAR_Y, CHR_NAMES)
+  PLOT <- make_plot(VCF, SEQ, VAR_FLAG, VAR_Y, CHR_NAMES)
   PLOT
 }
